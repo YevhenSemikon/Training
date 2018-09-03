1a9f588b4820628a039f59e446d41cbc6a12395d
Yevhen Semikon <YNSN@simcorp.com>
2018-09-03 16:24:42 +0300

    Add new classes DynamicMicrophone, Condenser microphone

--- MobilePhone/MobilePhoneLibrary/SimCorpMobilePhone.cs
+++ MobilePhone/MobilePhoneLibrary/SimCorpMobilePhone.cs
@@ -12,13 +12,13 @@ using MobilePhone.MobileComponents.SimCard;
 namespace MobilePhone {
     public class SimCorpMobilePhone : MobilePhone {
         public override ScreenBase Screen { get { return vOLEDScreen; } }
-        public override MicrophoneBase Microphone { get { return vNokiaMicrophone; } }
+       // public override MicrophoneBase Microphone { get { return vNokiaMicrophone; } }
         public override BatteryBase Battery { get { return vLiPolBattery; } }
         public override SpeakerBase Speaker { get { return vStereoSpeaker; } }
         public override SimCardBase SimCard { get { return vSimCard; } }
         private readonly OLEDScreen vOLEDScreen = new OLEDScreen();
-        private readonly NokiaMicrophone vNokiaMicrophone = new NokiaMicrophone();
-        private readonly LiPolBattery vLiPolBattery = new LiPolBattery(100);
+      //  private readonly NokiaMicrophone vNokiaMicrophone = new NokiaMicrophone();
+        private readonly LiPolBattery vLiPolBattery = new LiPolBattery();
         private readonly StereoSpeaker vStereoSpeaker = new StereoSpeaker(30);
         private readonly SimCard4g vSimCard = new SimCard4g ();
     }
