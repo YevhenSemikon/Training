736e187cd7eb20e493873357c27d22898982d6f9
Yevhen Semikon <YNSN@simcorp.com>
2018-09-10 13:45:29 +0300

    Revert "Task1-2"
    This reverts commit ad626bf89f7147dc6b0961b052fd192913cd0a1d.

--- MobilePhone/MobilePhoneProgram/Program.cs
+++ MobilePhone/MobilePhoneProgram/Program.cs
@@ -8,48 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MobilePhone.MobileComponents.Battery;
-using MobilePhone.MobileComponents.AudioJack;
 
 namespace MobilePhoneProgram {
     class Program {
         static void Main(string[] args){
-           SimCorpMobilePhone mobile = new SimCorpMobilePhone();
-           Console.WriteLine(mobile.GetDescription());
-            IPlayback playbackComponent=SelectPlaybackComponent();
-            mobile.PlaybackComponent = playbackComponent;
-            mobile.Play(mobile.PlaybackComponent);
-        }
-
-        private static IPlayback SelectPlaybackComponent()
-        {
-            IPlayback playbackComponent;
-            var text = new StringBuilder();
-            text.Append("Select playback component (specify index): " + System.Environment.NewLine);
-            text.Append("1 - iPhoneHeadset" + System.Environment.NewLine);
-            text.Append("2 - SamsungHeadset" + System.Environment.NewLine);
-            text.Append("3 - UnofficialiPhoneHeadset" + System.Environment.NewLine);
-            text.Append("4 - PhoneSpeaker");
-            Console.WriteLine(text.ToString());
-            string playbackIndex=Console.ReadLine();
-            switch (playbackIndex) {
-                case "1":
-                 playbackComponent = new iPhoneHeadset();
-                break;
-                case "2":
-                playbackComponent = new SamsungHeadset();
-                break;
-                case "3":
-                playbackComponent = new UnofficialiPhoneHeadset();
-                break;
-                case "4":
-                playbackComponent = new PhoneSpeaker();
-                break; 
-              default:
-                playbackComponent = new UnknownHeadset();
-                break;
-            }
-            Console.WriteLine(playbackComponent.ToString() + " playback selected");
-            return playbackComponent;
+            SimCorpMobilePhone mobile = new SimCorpMobilePhone();
+            Console.WriteLine(mobile.GetDescription());   
         }
     }
 }
